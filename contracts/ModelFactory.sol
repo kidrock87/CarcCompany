376cb16eb280f89120211e4c2121dae9dde9e146
kidrock87 <totalmmaru@gmail.com>
2022-02-28 23:30:18 +0300

    28.02

---
+++ contracts/ModelFactory.sol
@@ -0,0 +1,31 @@
+pragma solidity ^0.7.0;
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract ModelFactory is Ownable{
+
+  event NewModel(uint ModelId, string name, uint dna);
+
+  struct Model {
+        uint ModelId;
+        string name;
+        uint price;
+  }
+
+  Model[] public models;
+//when user buy car
+  mapping(address => Model) Orders;
+
+//meta is Now() from front when car creation start
+  function _createModel(string _meta ,uint _price,string _name) external onlyOwner{
+        uint ModelId = keccak256(_meta);
+        NewModel(ModelId, _name, _dna);
+    }
+  //get info about models
+  function getModels() public view returns (uint[] memory) {
+    return models;
+  }
+  //get info about car by ModelId
+  function _getModelInfo(){
+
+  }
+}
